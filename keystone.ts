4dca1e062a71fbe81f8b00e814a91aabc7edecf7
Aurélien Leygues <aurelien.leygues@gmail.com>
2021-06-29 12:17:26 +0200

    Update base app

--- keystone.ts
+++ keystone.ts
@@ -3,42 +3,43 @@ import { statelessSessions } from '@keystone-next/keystone/session';
 import { createAuth } from '@keystone-next/auth';
 import { lists } from './src/schema';
 
-let sessionSecret = process.env.SESSION_SECRET;
-
-if (!sessionSecret) {
-  if (process.env.NODE_ENV === 'production') {
-    throw new Error(
-      'The SESSION_SECRET environment variable must be set in production'
-    );
-  } else {
-    sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
-  }
-}
-
-let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
-
-const auth = createAuth({
-  listKey: 'User',
+// createAuth configures signin functionality based on the config below. Note this only implements
+// authentication, i.e signing in as an item using identity and secret fields in a list. Session
+// management and access control are controlled independently in the main keystone config.
+const { withAuth } = createAuth({
+  // This is the list that contains items people can sign in as
+  listKey: 'Person',
+  // The identity field is typically a username or email address
   identityField: 'email',
+  // The secret field must be a password type field
   secretField: 'password',
+
+  // initFirstItem turns on the "First User" experience, which prompts you to create a new user
+  // when there are no items in the list yet
   initFirstItem: {
+    // These fields are collected in the "Create First User" form
     fields: ['name', 'email', 'password'],
   },
 });
 
-export default auth.withAuth(
+// Stateless sessions will store the listKey and itemId of the signed-in user in a cookie.
+// This session object will be made available on the context object used in hooks, access-control,
+// resolvers, etc.
+const session = statelessSessions({
+  // The session secret is used to encrypt cookie data (should be an environment variable)
+  secret: '-- EXAMPLE COOKIE SECRET; CHANGE ME --',
+});
+
+// We wrap our config using the withAuth function. This will inject all
+// the extra config required to add support for authentication in our system.
+export default withAuth(
   config({
     db: {
-      adapter: 'prisma_postgresql',
-      url: process.env.DATABASE_URL || 'postgres://superuser:supersecret@db/dbname',
-    },
-    ui: {
-      isAccessAllowed: (context) => !!context.session?.data,
+      provider: 'sqlite',
+      url: process.env.DATABASE_URL || 'file:./keystone-example.db',
     },
     lists,
-    session: statelessSessions({
-      maxAge: sessionMaxAge,
-      secret: sessionSecret,
-    }),
+    // We add our session configuration to the system here.
+    session,
   })
 );
--- src/schema.ts
+++ src/schema.ts
@@ -1,99 +1,36 @@
 import { createSchema, list } from '@keystone-next/keystone/schema';
-import {
-  text,
-  relationship,
-  password,
-  timestamp,
-  select,
-} from '@keystone-next/fields';
-import { document } from '@keystone-next/fields-document';
+import { checkbox, password, relationship, text, timestamp } from '@keystone-next/fields';
+import { select } from '@keystone-next/fields';
 
 export const lists = createSchema({
-  User: list({
-    ui: {
-      listView: {
-        initialColumns: ['name', 'posts'],
-      },
+  Task: list({
+    fields: {
+      label: text({ isRequired: true }),
+      priority: select({
+        dataType: 'enum',
+        options: [
+          { label: 'Low', value: 'low' },
+          { label: 'Medium', value: 'medium' },
+          { label: 'High', value: 'high' },
+        ],
+      }),
+      isComplete: checkbox(),
+      assignedTo: relationship({ ref: 'Person.tasks', many: false }),
+      finishBy: timestamp(),
     },
+  }),
+  Person: list({
     fields: {
       name: text({ isRequired: true }),
+      // Added an email and password pair to be used with authentication
+      // The email address is going to be used as the identity field, so it's
+      // important that we set both isRequired and isUnique
       email: text({ isRequired: true, isUnique: true }),
+      // The password field stores a hash of the supplied password, and
+      // we want to ensure that all people have a password set, so we use
+      // the isRequired flag.
       password: password({ isRequired: true }),
-      posts: relationship({ ref: 'Post.author', many: true }),
-    },
-  }),
-  Post: list({
-    fields: {
-      title: text(),
-      status: select({
-        options: [
-          { label: 'Published', value: 'published' },
-          { label: 'Draft', value: 'draft' },
-        ],
-        ui: {
-          displayMode: 'segmented-control',
-        },
-      }),
-      content: document({
-        formatting: true,
-        layouts: [
-          [1, 2],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
-        ],
-        links: true,
-        dividers: true,
-      }),
-      publishDate: timestamp(),
-      author: relationship({
-        ref: 'User.posts',
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name', 'email'],
-          inlineEdit: { fields: ['name', 'email'] },
-          linkToItem: true,
-          inlineCreate: { fields: ['name', 'email'] },
-        },
-      }),
-      tags: relationship({
-        ref: 'Tag.posts',
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name'],
-          inlineEdit: { fields: ['name'] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ['name'] },
-        },
-        many: true,
-      }),
-    },
-  }),
-  Tag: list({
-    ui: {
-      isHidden: true,
-    },
-    fields: {
-      name: text({
-        ui: {
-
-        }
-      }),
-      posts: relationship({
-        ref: 'Post.tags',
-        many: true,
-        ui: {
-          //l
-        }
-      }),
-    },
-    hooks: {
-      afterChange: ({ operation, updatedItem, context }) => {
-        //context.db.lists.
-      }
+      tasks: relationship({ ref: 'Task.assignedTo', many: true }),
     },
   }),
 });
-
